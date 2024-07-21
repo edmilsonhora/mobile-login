36fe44058965fe667804f7fdbe820bc710f02bef
Edmilson Hora <edmilson_hora@yahoo.com.br>
2024-07-21 15:43:15 -0300

    add Sqlite

---
+++ src/ESH.MauiLogin/BackEnd/MyContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH.MauiLogin.BackEnd
+{
+    public class MyContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var db = Path.Combine(FileSystem.Current.AppDataDirectory, "myLogin.db");
+            optionsBuilder.UseSqlite($"Data Source={db}");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
